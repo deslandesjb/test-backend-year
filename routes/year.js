90f51729a746cbd1fa661a7d98bdbcde97e6d89c
Jb <deslandes.jb@gmail.com>
2025-10-31 11:15:11 +0100

    convert to string for ariane

--- routes/year.js
+++ routes/year.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 const date = new Date();
-const year = date.getFullYear();
+const year = date.getFullYear().toString();
 
 router.get("/", (req, res) => {
   res.json({result: true, year});
